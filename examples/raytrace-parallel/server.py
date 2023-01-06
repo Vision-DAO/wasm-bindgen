1e361131c12bde9451005b4624a0c3c8750cf6fe
Dowland Aiello <dowlandaiello@gmail.com>
2023-01-05 17:16:15 -0800

    Use SSL for raytrace example.

--- examples/raytrace-parallel/server.py
+++ examples/raytrace-parallel/server.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test, http
+import ssl
 import sys
+
 
 class RequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
-        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
-        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
         SimpleHTTPRequestHandler.end_headers(self)
 
-if __name__ == '__main__':
-    test(RequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+
+if __name__ == "__main__":
+    server_address = ("localhost", 3000)
+    httpd = http.server.HTTPServer(server_address, RequestHandler)
+    httpd.socket = ssl.wrap_socket(
+        httpd.socket,
+        server_side=True,
+        certfile="server.pem",
+        ssl_version=ssl.PROTOCOL_TLS,
+    )
+    httpd.serve_forever()
